05f0f47732618e40a62f00b62951cd885ec6db4b
Luiz Pires <lpiresjunior@gmail.com>
2021-03-26 18:32:03 -0300

    install styled components

---
+++ src/Home/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #000;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 44px;
+`;
